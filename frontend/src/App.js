0e2b020699c69187e8fac758891c8de1e2c75ba4
Ramzi Salem <ramzysa96@gmail.com>
2019-12-01 13:49:29 +0100

    test backend-deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 import withFirebaseAuth from 'react-with-firebase-auth';
 import firebase from 'firebase'
 import 'firebase/auth';
@@ -14,11 +22,20 @@ const providers = {
 };
 
 
+
+
 function App() {
   return (
-<div>
-  <h1>IT'S WORKING</h1>
-</div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
