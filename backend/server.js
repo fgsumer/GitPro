8f786a4e0e31ed6bd46ed20101658127560fd014
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-12-07 00:51:41 +0100

    feat: add routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // body parser middleware
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
@@ -18,9 +18,15 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const workExperienceRouter = require('./routes/workExperience');
+const skillRouter = require('./routes/skill');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+app.use('/work', workExperienceRouter);
+app.use('/skill', skillRouter);
+app.use('/auth', authRouter);
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
